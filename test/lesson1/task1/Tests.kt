5ba1d13f9d8168a56bd5d531a1e39cd3902fd157
KazaxxX <p.lexa.99@mail.ru>
2017-10-04 17:31:48 +0500

    Lesson 1

--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -56,6 +56,7 @@ class Tests {
     fun angleInRadian() {
         assertEquals(0.63256, angleInRadian(36, 14, 35), 1e-5)
         assertEquals(Math.PI / 2.0, angleInRadian(90, 0, 0), 1e-5)
+        assertEquals(Math.PI / 2.0, angleInRadian(90, 0, 0), 1e-5)
     }
 
     @Test
